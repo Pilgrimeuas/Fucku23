446ebede8da1010d33cb75accebd508ad9314339
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-01-06 09:35:31 +0000

    better && claim unserialized bytes

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/BedrockPacket.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/BedrockPacket.java
@@ -3,12 +3,15 @@ package com.nukkitx.protocol.bedrock;
 import com.nukkitx.protocol.MinecraftPacket;
 import com.nukkitx.protocol.bedrock.handler.BedrockPacketHandler;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 public abstract class BedrockPacket implements MinecraftPacket {
     private int packetId;
     private int senderId;
     private int clientId;
+    @Setter
+    private byte[] rawData = null;
 
     public abstract boolean handle(BedrockPacketHandler handler);
 
--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/BedrockPacketCodec.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/BedrockPacketCodec.java
@@ -1,29 +1,26 @@
 package com.nukkitx.protocol.bedrock;
 
+
 import com.nukkitx.protocol.bedrock.exception.PacketSerializeException;
 import com.nukkitx.protocol.bedrock.packet.UnknownPacket;
+import com.nukkitx.protocol.util.Int2ObjectBiMap;
 import io.netty.buffer.ByteBuf;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.lanternpowered.lmbda.LambdaFactory;
-import org.lanternpowered.lmbda.MethodHandlesExtensions;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.util.IdentityHashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 
-import static com.nukkitx.network.util.Preconditions.*;
+import static com.nukkitx.network.util.Preconditions.checkArgument;
+import static com.nukkitx.network.util.Preconditions.checkNotNull;
 
 @Immutable
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -34,73 +31,70 @@ public final class BedrockPacketCodec {
     private final int protocolVersion;
     @Getter
     private final String minecraftVersion;
-    private final BedrockPacketDefinition<? extends BedrockPacket>[] packetsById;
-    private final Map<Class<? extends BedrockPacket>, BedrockPacketDefinition<? extends BedrockPacket>> packetsByClass;
+    private final BedrockPacketSerializer<BedrockPacket>[] serializers;
+    private final Int2ObjectBiMap<Class<? extends BedrockPacket>> idBiMap;
     @Getter
     private final BedrockPacketHelper helper;
     @Getter
     private final int raknetProtocolVersion;
 
+    public boolean hasDecodeFailure = false;
+
     public static Builder builder() {
         return new Builder();
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
     public BedrockPacket tryDecode(ByteBuf buf, int id, BedrockSession session) throws PacketSerializeException {
-        BedrockPacketDefinition<? extends BedrockPacket> definition = getPacketDefinition(id);
         BedrockPacket packet;
         BedrockPacketSerializer<BedrockPacket> serializer;
-        if (definition == null) {
-            UnknownPacket unknownPacket = new UnknownPacket();
-            unknownPacket.setPacketId(id);
-            packet = unknownPacket;
-            serializer = (BedrockPacketSerializer) unknownPacket;
-        } else {
-            packet = definition.getFactory().get();
-            serializer = (BedrockPacketSerializer) definition.getSerializer();
+        try {
+            packet = idBiMap.get(id).newInstance();
+            if (packet instanceof UnknownPacket) {
+                //noinspection unchecked
+                serializer = (BedrockPacketSerializer<BedrockPacket>) packet;
+            } else {
+                serializer = serializers[id];
+            }
+        } catch (ClassCastException | InstantiationException | IllegalAccessException | ArrayIndexOutOfBoundsException e) {
+            throw new PacketSerializeException("Packet ID " + id + " does not exist", e);
         }
 
         try {
             serializer.deserialize(buf, this.helper, packet, session);
         } catch (Exception e) {
+            hasDecodeFailure = true;
             throw new PacketSerializeException("Error whilst deserializing " + packet, e);
         }
 
         if (log.isDebugEnabled() && buf.isReadable()) {
             log.debug(packet.getClass().getSimpleName() + " still has " + buf.readableBytes() + " bytes to read!");
+            final byte[] rawData = new byte[buf.readableBytes()];
+            buf.readBytes(rawData);
+            packet.setRawData(rawData);
         }
         return packet;
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings("unchecked")
     public void tryEncode(ByteBuf buf, BedrockPacket packet, BedrockSession session) throws PacketSerializeException {
         try {
             BedrockPacketSerializer<BedrockPacket> serializer;
             if (packet instanceof UnknownPacket) {
                 serializer = (BedrockPacketSerializer<BedrockPacket>) packet;
             } else {
-                BedrockPacketDefinition definition = getPacketDefinition(packet.getClass());
-                serializer = definition.getSerializer();
+                int packetId = getId(packet.getClass());
+                serializer = serializers[packetId];
             }
             serializer.serialize(buf, this.helper, packet, session);
+            final byte[] rawData = packet.getRawData();
+            if (rawData != null) {
+                buf.writeBytes(rawData);
+            }
         } catch (Exception e) {
             throw new PacketSerializeException("Error whilst serializing " + packet, e);
         } finally {
             ReferenceCountUtil.release(packet);
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T extends BedrockPacket> BedrockPacketDefinition<T> getPacketDefinition(Class<T> packet) {
-        checkNotNull(packet, "packet");
-        return (BedrockPacketDefinition<T>) packetsByClass.get(packet);
-    }
-
-    public BedrockPacketDefinition<? extends BedrockPacket> getPacketDefinition(int id) {
-        if (id < this.packetsById.length) {
-            return this.packetsById[id];
-        }
-        return null;
     }
 
     public int getId(BedrockPacket packet) {
@@ -111,17 +105,20 @@ public final class BedrockPacketCodec {
     }
 
     public int getId(Class<? extends BedrockPacket> clazz) {
-        BedrockPacketDefinition<?> definition = getPacketDefinition(clazz);
-        if (definition == null) {
+        int id = idBiMap.get(clazz);
+        if (id == -1) {
             throw new IllegalArgumentException("Packet ID for " + clazz.getName() + " does not exist.");
         }
-        return definition.getId();
+        return id;
     }
 
+    @SuppressWarnings("unchecked")
     public Builder toBuilder() {
         Builder builder = new Builder();
 
-        builder.packets.putAll(this.packetsByClass);
+        idBiMap.forEach((packetClass, id) ->
+                builder.registerPacket((Class<BedrockPacket>) packetClass, this.serializers[id], id));
+
         builder.protocolVersion = this.protocolVersion;
         builder.raknetProtocolVersion = this.raknetProtocolVersion;
         builder.minecraftVersion = this.minecraftVersion;
@@ -133,37 +130,29 @@ public final class BedrockPacketCodec {
     @SuppressWarnings("unchecked")
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Builder {
-        private final Map<Class<? extends BedrockPacket>, BedrockPacketDefinition<? extends BedrockPacket>> packets = new IdentityHashMap<>();
+        private final Int2ObjectMap<BedrockPacketSerializer<BedrockPacket>> serializers = new Int2ObjectOpenHashMap<>();
+        private final Int2ObjectBiMap<Class<? extends BedrockPacket>> idBiMap = new Int2ObjectBiMap<>(UnknownPacket.class);
         private int protocolVersion = -1;
         private int raknetProtocolVersion = 10;
         private String minecraftVersion = null;
         private BedrockPacketHelper helper = null;
 
-        @SuppressWarnings({"ConstantConditions", "rawtypes"})
         public <T extends BedrockPacket> Builder registerPacket(Class<T> packetClass, BedrockPacketSerializer<T> serializer, @Nonnegative int id) {
             checkArgument(id >= 0, "id cannot be negative");
-            checkArgument(!this.packets.containsKey(packetClass), "Packet class already registered");
+            checkArgument(!idBiMap.containsKey(id), "Packet id already registered");
+            checkArgument(!idBiMap.containsValue(packetClass), "Packet class already registered");
 
-            Supplier<Object> factory;
-            try {
-                MethodHandles.Lookup lookup = MethodHandlesExtensions.privateLookupIn(packetClass, MethodHandles.lookup());
-                MethodHandle handle = lookup.findConstructor(packetClass, MethodType.methodType(void.class));
-                factory = LambdaFactory.createSupplier(handle);
-            } catch (NoSuchMethodException | IllegalAccessException e) {
-                throw new IllegalArgumentException("Unable to find suitable constructor for packet factory", e);
-            }
-
-            BedrockPacketDefinition<T> info = new BedrockPacketDefinition<>(id, (Supplier) factory, serializer);
-
-            this.packets.put(packetClass, info);
-
+            serializers.put(id, (BedrockPacketSerializer<BedrockPacket>) serializer);
+            idBiMap.put(id, packetClass);
             return this;
         }
 
         public void deregisterPacket(Class<? extends BedrockPacket> packetClass) {
             checkNotNull(packetClass, "packetClass");
-
-            BedrockPacketDefinition<? extends BedrockPacket> info = packets.remove(packetClass);
+            if (!this.idBiMap.containsValue(packetClass)) return;
+            int packetId = this.idBiMap.get(packetClass);
+            this.idBiMap.remove(packetClass);
+            this.serializers.remove(packetId);
         }
 
         public Builder protocolVersion(@Nonnegative int protocolVersion) {
@@ -196,18 +185,18 @@ public final class BedrockPacketCodec {
             checkNotNull(minecraftVersion, "No Minecraft version defined");
             checkNotNull(helper, "helper cannot be null");
             int largestId = -1;
-            for (BedrockPacketDefinition<? extends BedrockPacket> info : packets.values()) {
-                if (info.getId() > largestId) {
-                    largestId = info.getId();
+            for (int id : serializers.keySet()) {
+                if (id > largestId) {
+                    largestId = id;
                 }
             }
             checkArgument(largestId > -1, "Must have at least one packet registered");
-            BedrockPacketDefinition<? extends BedrockPacket>[] packetsById = new BedrockPacketDefinition[largestId + 1];
+            BedrockPacketSerializer<BedrockPacket>[] serializers = new BedrockPacketSerializer[largestId + 1];
 
-            for (BedrockPacketDefinition<? extends BedrockPacket> info : packets.values()) {
-                packetsById[info.getId()] = info;
+            for (Int2ObjectMap.Entry<BedrockPacketSerializer<BedrockPacket>> entry : this.serializers.int2ObjectEntrySet()) {
+                serializers[entry.getIntKey()] = entry.getValue();
             }
-            return new BedrockPacketCodec(protocolVersion, minecraftVersion, packetsById, packets, helper, raknetProtocolVersion);
+            return new BedrockPacketCodec(protocolVersion, minecraftVersion, serializers, idBiMap, helper, raknetProtocolVersion);
         }
     }
 }
